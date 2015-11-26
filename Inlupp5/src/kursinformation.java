4b3ac341f919891be3a063287df9eacfae17a31a
merryaiiit <merryaiiit@gmail.com>
2015-11-26 16:05:48 +0100

    Create kursinformation.java
    information om kurser

---
+++ Inlupp5/src/kursinformation.java
@@ -0,0 +1,48 @@
+### Languages 5 hp ###
+Albanian = Kemal  - How do you say cow in Albanian?
+
+Spanish  = Jose - What does "el pojo loco" mean?
+
+German = Hans - Mädchen means girl in german, but how do you say boy in german?
+
+Italian = Bella - What does "grazie" mean in italian?
+
+French - Abel - Where is the famous Eiffeltower located?
+
+English - Harry - In what country do men wear kilts?
+
+Latin - Da vinci - What is femur?
+
+Chinese - Sum ting wong - What is the capitalcity of China?
+ 
+
+### Sports  10 hp ###
+Basketball - James - In what team was Michael Jordan at his best?
+
+Swimming - Sandra - How many gold-medals did Michael Phelps get in 2008 Summer Olympics
+
+Track and field - Adam - Usain Bolt broke the WR in 100m, how fast did he run when he broke it?
+
+Hockey - Karolina - Peter Forsberg is a famous Swedish hockey-player, what is his nickname?
+
+Fotball - Totti - Which spanishteam did Zlatan Ibrahimovic play in?
+
+### Norml Courses = 20 hp ###
+Computer science - Tobias - How much is 0110 decimal?
+
+Algebra Robert - What is the answer of: x+2 = 3
+
+Geometry - Per - How many corner does a hexagon have?
+
+History - Kicki - When did they remove the Berlin-Wall?
+
+Economy - Britta - What is the name of the founder company IKEA?
+
+Chemistry - Lothar - What is CO2? 
+
+Physics  - Mats - What does I stand for in the formula I=U/R
+
+
+
+
+Room 1
